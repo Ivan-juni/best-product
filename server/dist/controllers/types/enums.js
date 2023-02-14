fbbec13b2fd0b7f16fc51c4e9d84d82ecba452bb
Ivan-juni <ivanuk.vanya@gmail.com>
2023-02-14 13:58:21 +0200

    chore: removed dist folder

--- server/dist/controllers/types/enums.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ROLES = void 0;
-var ROLES;
-(function (ROLES) {
-    ROLES["ADMIN"] = "ADMIN";
-    ROLES["USER"] = "USER";
-})(ROLES = exports.ROLES || (exports.ROLES = {}));
-//# sourceMappingURL=enums.js.map
--- server/dist/utils/replace-spaces.util.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.replaceSpaces = void 0;
-// replace the spaces with underlines
-const replaceSpaces = (folder) => folder.replace(/\s/g, '_');
-exports.replaceSpaces = replaceSpaces;
-//# sourceMappingURL=replace-spaces.util.js.map
